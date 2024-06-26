f436d8bea608623b6a1ffa3bd55a377647548f5b
Adrian Flores <140359838+CarlosAdrian25@users.noreply.github.com>
2024-06-26 19:58:26 +0000

    commit

--- reportes.py
+++ reportes.py
@@ -21,9 +21,24 @@ def mostrar_pagina_visualizacion():
 
     # creacion de data view de grados de texto
 
-    st.metric(label="Ventas", value=hola[0], delta=f"{5} %")
+    # st.metric(label="Ventas", value=hola[0], delta=f"{5} %")
 
     col1,col2,col3=st.columns(3)
     col1.metric("Ventas", hola[0])
     col2.metric("Secciones",hola[0])
     col3.metric("Promedio", hola[0], delta=f"{5} %")
+
+    #radio de select box
+
+    options = st.multiselect(
+    "Seleccione uno o mas productos",
+    ["Green", "Yellow", "Red", "Blue"],
+    ["Yellow", "Red"])
+
+    st.write("You selected:", options)
+
+    evo = st.selectbox(
+    "Select Box",
+    ("Email", "Home phone", "Mobile phone")
+    )
+    st.write("You selected:", evo)
