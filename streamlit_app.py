9c6b81b350e56c681cdcba1f84450c6e57f35d30
Adrian Flores <140359838+CarlosAdrian25@users.noreply.github.com>
2024-06-29 05:12:36 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,31 @@ from reportes import mostrar_pagina_visualizacion
 from archivos import archivo
 from analisis import analisis
 from Ayuda import ayuda
+from streamlit_option_menu import option_menu
 
-# Menú de la aplicacion con streamlit
-st.sidebar.title("MENU DE REPORTES ESTADISTICOS")
-menu_opciones=st.sidebar.selectbox("Selecciona una opción", ["Carga de Datos", "Visualización de Datos", "Análisis", "Ayuda"])
-if menu_opciones=='Carga de Datos':
-    archivo()
-elif menu_opciones=="Visualización de Datos":
-    mostrar_pagina_visualizacion()
-elif menu_opciones=="Análisis":
-    analisis()
-elif menu_opciones=="Ayuda":
-    ayuda()
+# Crear el menú con iconos
+with st.sidebar:
+    selected = option_menu(
+        "MENU",
+        ["ANALISIS", "SUBIR ARCHIVO", "REPORTES (EXPORTACION)"],
+        icons=["wrench", "cloud", "lightbulb"],
+        menu_icon="cast",
+        default_index=1,
+        styles={
+            "container": {"padding": "5px", "background-color": "#f0f0f0"},
+            "icon": {"color": "orange", "font-size": "25px"}, 
+            "nav-link": {"font-size": "16px", "text-align": "left", "margin": "0px", "--hover-color": "#eee"},
+            "nav-link-selected": {"background-color": "blue"},
+        }
+    )
 
+# Mostrar contenido basado en la selección
+if selected == "Home":
+    st.write("Esta es la página de inicio")
+elif selected == "Settings":
+    st.write("Esta es la página de configuración")
+elif selected == "About":
+    st.write("Esta es la página de acerca de")
 
 
 
