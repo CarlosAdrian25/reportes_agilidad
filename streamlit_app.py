db0b27906304805ea7315c7044784f8525e4db28
Adrian Flores <140359838+CarlosAdrian25@users.noreply.github.com>
2024-06-30 04:26:01 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,28 +5,43 @@ from analisis import analisis
 from Ayuda import ayuda
 from streamlit_option_menu import option_menu
 
-# Crear el menú con iconos
+st.markdown(
+    """
+    <style>
+    [data-testid="stSidebar"] > div:first-child {
+        background-color: #1a1a2e;
+    }
+    .menu_icon {
+        color: #ffd700;  /* Color dorado */
+        font-size: 25px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 with st.sidebar:
+    titulo=st.markdown("<center><h1 style='color: #ffd590;'>🗳 PANEL DE CONTROL</h1></center>", unsafe_allow_html=True)
     selected = option_menu(
-        "MENU",
+        "",
         ["ANALISIS", "SUBIR ARCHIVO", "REPORTES (EXPORTACION)"],
         icons=["wrench", "cloud", "lightbulb"],
         menu_icon="cast",
         default_index=1,
         styles={
-            "container": {"padding": "5px", "background-color": "#f0f0f0"},
-            "icon": {"color": "orange", "font-size": "25px"}, 
-            "nav-link": {"font-size": "16px", "text-align": "left", "margin": "0px", "--hover-color": "#eee"},
-            "nav-link-selected": {"background-color": "blue"},
+            "container": {"padding": "5px", "background-color": "#1a1a2e","color":"white"},
+            "icon": {"color": "#ffd700", "font-size": "25px"}, 
+            "nav-link": {"font-size": "16px", "text-align": "left", "margin": "0px", "color": "#eaeaea", "--hover-color": "#0f3460"},
+            "nav-link-selected": {"background-color": "#16213e"},
         }
     )
 
 # Mostrar contenido basado en la selección
-if selected == "Home":
+if selected == "ANALISIS":
     st.write("Esta es la página de inicio")
-elif selected == "Settings":
-    st.write("Esta es la página de configuración")
-elif selected == "About":
+elif selected == "SUBIR ARCHIVO":
+    archivo()
+elif selected == "REPORTES (EXPORTACION)":
     st.write("Esta es la página de acerca de")
 
 
